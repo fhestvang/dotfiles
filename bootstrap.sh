4b9ac28a398314978eb8082c7b4a624ebff406b2
Frederik Hestvang <frederikh@tradingdesk.dk>
2018-02-27 13:58:48 +0100

    --amend

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".zshrc .vimrc .vim"        # list of files/folders to symlink in homedir
+files=".zshrc .vimrc .vim .tmux.conf"        # list of files/folders to symlink in homedir
 
 ##########
 
